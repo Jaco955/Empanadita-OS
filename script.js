943e475bc2348e4e8f6b1c0d719cf1a7c3f7bed8
Jaco955 <jacotareas@gmail.com>
2026-09-19 23:29:43 +0000

    Made closing buttons

---
+++ script.js
@@ -0,0 +1,92 @@
+function updateTime() {
+   var PacTheMan = new Date(). toLocaleString();
+   var timeText = document.querySelector("#timeElement");
+   timeText.innerHTML = PacTheMan
+}
+ setInterval(updateTime, 1000)
+
+ dragElement(document.getElementById("welcome"));
+
+function dragElement(element) {
+
+  var initialX = 0;
+  var initialY = 0;
+  var currentX = 0;
+  var currentY = 0;
+
+ 
+  if (document.getElementById(element.id + "header")) {
+ 
+    document.getElementById(element.id + "header").onmousedown = startDragging;
+  } else {
+ 
+    element.onmousedown = startDragging;
+  }
+
+   function startDragging(e) {
+    e = e || window.event;
+    e.preventDefault();
+ 
+    initialX = e.clientX;
+    initialY = e.clientY;
+ 
+    document.onmouseup = stopDragging;
+    document.onmousemove = dragElement;
+  }
+
+ 
+  function dragElement(e) {
+    e = e || window.event;
+    e.preventDefault();
+ 
+    currentX = initialX - e.clientX;
+    currentY = initialY - e.clientY;
+    initialX = e.clientX;
+    initialY = e.clientY;
+ 
+    element.style.top = (element.offsetTop - currentY) + "px";
+    element.style.left = (element.offsetLeft - currentX) + "px";
+  }
+
+ 
+  function stopDragging() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  } 
+}
+
+dragElement(document.getElementById("welcome"));
+dragElement(document.getElementById("foo-fighters"));
+dragElement(document.getElementById("clicky-things"));
+dragElement(document.getElementById("rawr"));
+
+var welcomeScreen = document.querySelector("#welcome")
+var foofightersScreen = document.querySelector("#foo-fighters")
+var clickyScreen = document.querySelector("#clicky-things")
+
+function openWindow(element) {
+  element.style.display = "flex"
+}
+function closeWindow(element) {
+  element.style.display = "none"
+}
+
+var welcomeScreenOpen = document.querySelector("#welcomeopen")
+var welcomeScreenClose = document.querySelector("#welcomeclose")
+
+welcomeScreenOpen.addEventListener("click", function() {
+  openWindow(welcomeScreen);
+});
+welcomeScreenClose.addEventListener("click", function() {
+  closeWindow(welcomeScreen);
+});
+
+var foofightersScreenOpen = document.querySelector("#welcomeopen")
+var foofightersScreenClose = document.querySelector("#foo-fightersclose")
+
+foofightersScreenOpen.addEventListener("click", function() {
+  openWindow(foofightersScreen);
+});
+foofightersScreenClose.addEventListener("click", function() {
+  closeWindow(foofightersScreen);
+});
